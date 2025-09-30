8f916a3e54e1debd406f698fadc59777bc3c4750
ataur <atauraahmad@gmail.com>
2025-09-30 01:05:27 +0100

    add secs funtionality

--- app.js
+++ app.js
@@ -5,13 +5,29 @@ const stopBtn = document.querySelector(".stop");
 const resetBtn = document.querySelector(".reset");
 const startBtn = document.querySelector(".start");
 
+console.log(typeof 0);
+let s = 0;
+let m = 0;
+let h = 0;
+
 stopBtn.addEventListener("click", () => {
   console.log("stop");
 });
 startBtn.addEventListener("click", () => {
   console.log("start");
+
+  secIncrement(s);
 });
 resetBtn.addEventListener("click", () => {
-  display.textContent = "00:00:00";
+  display.textContent = `00:00:00`;
   console.log("reseted");
 });
+
+const secIncrement = (s) => {
+  let news;
+  setInterval(() => {
+    news = s++;
+    display.textContent = `00:00:${news}`;
+    return news;
+  }, 1000);
+};
